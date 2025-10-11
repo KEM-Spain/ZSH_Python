78d659dd1ff4dc1b9ca185b3f44e61e4349d3dc6
Kurt Miller <miller.kurt.e@gmail.com>
2025-10-11 10:09:53 +0200

    10-11-2025-10:09:52

--- ZSH_LIB/ERROR.zsh
+++ ZSH_LIB/ERROR.zsh
@@ -3,19 +3,13 @@ _DEPS_+=(DBG.zsh)
 
 # LIB functions
 err_msg_exit () {
-	if [[ ${#} -eq 2 ]];then
-		local E_TYPE=${1}
-		local MSG=${2}
-	else
-		return 1
-	fi
+	local E_TYPE=${1}
+	local E_MSG=${2}
 
 	local LABEL=''
 	local LCOLOR=''
 
 	[[ ${_DEBUG} -ge ${_MID_DBG} ]] && dbg "${functrace[1]} called ${0}:${LINENO}: ARGC:${#@}"
-
-	[[ -z ${@} ]] && (echo "${0}: missing argument" && return 1)
 
 	case ${E_TYPE} in 
 		W) LABEL="Warning";LCOLOR=${ITALIC}${BOLD}${MAGENTA_FG};;
@@ -24,9 +18,8 @@ err_msg_exit () {
 		*) LABEL="Unknown E_TYPE:${E_TYPE}";;
 	esac
 
-	if [[ ${MSG} != 'null' ]];then
-		[[ ${MSG} =~ ":" ]] && MSG=$(perl -pe "s/^(.*:)(.*)$/\1\e[37m\2\e[m/" <<<${MSG})
-		printf "[${WHITE_FG}%s${RESET}]:[${LCOLOR}${LABEL}${RESET}] %s" ${_SCRIPT} "$(echo ${MSG})" >&2
+	if [[ ${E_MSG} != 'null' ]];then
+		[[ ${E_MSG} =~ ":" ]] && E_MSG=$(perl -pe "s/^(.*:)(.*)$/\1\e[37m\2\e[m/" <<<${E_MSG})
+		printf "[${WHITE_FG}%s${RESET}]:[${LCOLOR}${LABEL}${RESET}] %s" ${_SCRIPT} "$(echo ${E_MSG})" >&2
 	fi
-	return 0
 }
