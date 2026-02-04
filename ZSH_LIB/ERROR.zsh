edb8bce9c1343b206069dd4c05afa4deb1f5f029
Kurt Miller <miller.kurt.e@gmail.com>
2026-02-04 14:00:05 +0100

    02-04-2026-14:00:04

--- ZSH_LIB/ERROR.zsh
+++ ZSH_LIB/ERROR.zsh
@@ -10,17 +10,22 @@ err_msg_exit () {
 
 	[[ ${_DEBUG} -ge ${_MID_DBG} ]] && dbg "${functrace[1]} called ${0}:${LINENO}: ARGC:${#@}"
 
-	if [[ ${#} -eq 1 ]];then
-		E_MSG=${1}
-		if [[ ${#E_MSG} -eq 1 && ! ${E_MSG} =~ 'W|E|I' ]];then
-			return 1 # No message passed
-		else
-			E_TYPE=E # Default type
+	if [[ ! -t 0 ]];then
+		read E_MSG
+		E_TYPE=E
+	else
+		if [[ ${#} -eq 1 ]];then
 			E_MSG=${1}
+			if [[ ${#E_MSG} -eq 1 && ! ${E_MSG} =~ '^(W|E|I)$' ]];then
+				return 1 # No message passed
+			else
+				E_TYPE=E # Default type
+				E_MSG=${1}
+			fi
+		elif [[ ${#} -eq 2 ]];then
+			E_TYPE=${1}
+			E_MSG=${2}
 		fi
-	elif [[ ${#} -eq 2 ]];then
-		E_TYPE=${1}
-		E_MSG=${2}
 	fi
 
 	case ${E_TYPE} in 
@@ -30,7 +35,7 @@ err_msg_exit () {
 		*) LABEL="Unknown E_TYPE:${E_TYPE}";;
 	esac
 
-	[[ ${E_MSG} =~ ":" ]] && E_MSG=$(perl -pe "s/^(.*:)(.*)$/\1\e[37m\2\e[m/" <<<${E_MSG})
+	[[ ${E_MSG} =~ ":" ]] && E_MSG=$(perl -pe "s/^(.*:)(\w+)(.*)$/\1\e[37m\2\e[m\3/" <<<${E_MSG})
 
 	printf "[${WHITE_FG}%s${RESET}]:[${LCOLOR}${LABEL}${RESET}] %s" ${_SCRIPT} "$(echo ${E_MSG})"
 }
