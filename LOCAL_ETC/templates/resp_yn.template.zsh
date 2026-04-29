6711c782ef910bd12ee3279564f6e79d6bfe30b4
Kurt Miller <miller.kurt.e@gmail.com>
2026-04-29 12:11:25 +0200

    04-29-2026-12:11:23

--- LOCAL_ETC/templates/resp_yn.template.zsh
+++ LOCAL_ETC/templates/resp_yn.template.zsh
@@ -1,14 +1,8 @@
 # y/n response template
-local ALERT="${RED_FG}Warning${RESET}"
-local DESCRIPTION="ACTION"
-
-echo -n "\n${RED_FG}${ALERT}!${RESET} ${DESCRIPTION}${WHITE_FG}${RESET}:(${WHITE_FG}y/n${RESET})?"
+QUESTION="${WHITE_FG}This action is pending${RESET}"
+echo -n "\n${QUESTION}:(y/n)?"
 read -q RESPONSE
 echo
-if [[ ${RESPONSE} == "y" ]];then # Only 'y' will execute task
-	echo "${WHITE_FG}${DESCRIPTION}${RESET}"
-	echo "ACTION goes here"
-else # All other keys terminate
-	echo "${RED_FG}Operation cancelled${RESET}..."
-	exit
+if [[ ${RESPONSE} != "y" ]];then # Only 'y' will not exit
+	exit_leave "${RED_FG}Operation cancelled${RESET}..."
 fi
