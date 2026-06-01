026e1fc50a7e95495fe9086b651bfe5bbfa0a29d
Kurt Miller <miller.kurt.e@gmail.com>
2026-06-01 12:35:37 +0200

    06-01-2026-12:35:36

--- LOCAL/scratch/lib_init/LIB_DEPS.zsh
+++ LOCAL/scratch/lib_init/LIB_DEPS.zsh
@@ -1,28 +1,38 @@
 typeset -a APP_DEPS=(${@})
 typeset -A SEEN=()
+DBG=true
 
-#echo "ARGS:${#} ${@}" > debug.log
-#echo "APP_DEPS:${APP_DEPS}" >> debug.log
+is_valid () {
+	FN=${1}
+
+	[[ ! -e ${_LIB_DIR}/${FN} ]] && return 1 # Don't source non-existent
+	[[ ${SEEN[${FN}]} -eq 1 ]] && return 1 # Don't source seen
+	[[ ${FN} == ${0} ]] && return 1 # Don't source ourselves
+	return 0
+}
+
+[[ ${DBG} == 'true' ]] && echo "ARGS:${#} ${@}" > debug.log
+[[ ${DBG} == 'true' ]] && echo "APP_DEPS:${APP_DEPS}" >> debug.log
 
 # Pre-scan - each APP module can add it's dependencies 
 # to _DEPS (defined in LIB_INIT) to be sourced by the post scan
 for A in ${=APP_DEPS};do # Pre scan
-	[[ ${A} == ${0:t} ]] && continue # Don't source ourselves
-	[[ ${SEEN[${A}]} -eq 1 ]] && continue # Don't source seen
-	[[ ! -e ${_LIB_DIR}/${A} ]] && continue # Don't source non-existent
-	#echo "Pre-scan Sourcing ${A}" >> debug.log
-	source ${_LIB_DIR}/${A}
-	SEEN[${A}]=1
+	FN=${A:t}
+	if is_valid ${FN};then
+		[[ ${DBG} == 'true' ]] && echo "Pre-scan Sourcing ${FN}" >> debug.log
+		source ${_LIB_DIR}/${FN}
+		SEEN[${FN}]=1
+	fi
 done
 
-#echo "_DEPS:${_DEPS}" >> debug.log
+[[ ${DBG} == 'true' ]] && echo "_DEPS:${_DEPS}" >> debug.log
 
 # Post-scan - source any modules in _DEPS
 for D in ${_DEPS};do # Post scan
-	[[ ${D} == ${0:t} ]] && continue # Don't source ourselves
-	[[ ${SEEN[${D}]} -eq 1 ]] && continue # Don't source seen
-	[[ ! -e ${_LIB_DIR}/${D} ]] && continue # Don't source non-existent
-	#echo "Post-scan Sourcing ${D}" >> debug.log
-	source ${_LIB_DIR}/${D}
-	SEEN[${D}]=1
+	FN=${D:t}
+	if is_valid ${FN};then
+		[[ ${DBG} == 'true' ]] && echo "Post-scan Sourcing ${FN}" >> debug.log
+		source ${_LIB_DIR}/${FN}
+		SEEN[${FN}]=1
+	fi
 done
--- LOCAL_ETC/templates/import.template.zsh
+++ LOCAL_ETC/templates/import.template.zsh
@@ -1,4 +1,7 @@
+typeset -a DEPS=()
+
 # Imports
 [[ -z ${ZSH_LIB_DIR} ]] && _LIB_DIR=/usr/local/lib || _LIB_DIR=${ZSH_LIB_DIR}
 source ${_LIB_DIR}/LIB_INIT.zsh
-source ${_LIB_DIR}/LIB_DEPS.zsh ${DEPS} # (Must be pre defined array and loaded with dependencies or passed as string)
+source ${_LIB_DIR}/LIB_DEPS.zsh ${DEPS}
+
