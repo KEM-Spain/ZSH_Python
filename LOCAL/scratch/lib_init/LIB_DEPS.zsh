0773f28cb0726238c52f8fd8c1ea82af21027636
Kurt Miller <miller.kurt.e@gmail.com>
2026-06-03 14:28:37 +0200

    06-03-2026-14:28:36

--- LOCAL/scratch/lib_init/LIB_DEPS.zsh
+++ LOCAL/scratch/lib_init/LIB_DEPS.zsh
@@ -1,5 +1,5 @@
 typeset -A SEEN=()
-DBG=false
+_DBG=false
 
 is_valid () {
 	FN=${1}
@@ -10,22 +10,22 @@ is_valid () {
 	return 0
 }
 
-[[ ${DBG} == 'true' ]] && echo "ARGS:${#} ${@}" > debug.log
-[[ ${DBG} == 'true' ]] && echo "APP_DEPS:${APP_DEPS}" >> debug.log
-
-# Multiple passes may be needed as previously
-# unseen dependencies are added to _DEPS array
+# LIB_INIT defines the _DEPS array and pre-populates
+# it with default modules. Sourced modules then
+# append _DEPS with their dependencies.  Multiple 
+# passes resolve any additions to the _DEPS array if 
+# modified when modules are sourced
 MAX_DEPS=${#_DEPS}
 PASS=1
 while true;do
 	for D in ${_DEPS};do # Post scan
 		FN=${D:t}
 		if is_valid ${FN};then
-			[[ ${DBG} == 'true' ]] && echo "Dependency scan pass ${PASS}: Sourcing ${FN}" >> debug.log
+			[[ ${_DBG} == 'true' ]] && echo "Dependency scan pass ${PASS}: Sourcing ${FN}" >> debug.log
 			source ${_LIB_DIR}/${FN}
 			SEEN[${FN}]=1
 		fi
 	done
-	[[ ${#_DEPS} -gt ${MAX_DEPS} ]] && MAX_DEPS=${#_DEPS} || break
+	[[ ${#_DEPS} -gt ${MAX_DEPS} ]] && MAX_DEPS=${#_DEPS} || break # _DEPS is stable - break
 	((PASS++))
 done
--- ZSH_LIB/LIB_DEPS.zsh
+++ ZSH_LIB/LIB_DEPS.zsh
@@ -1,6 +1,5 @@
-typeset -a APP_DEPS=(${@})
 typeset -A SEEN=()
-DBG=false
+_DBG=false
 
 is_valid () {
 	FN=${1}
@@ -11,38 +10,22 @@ is_valid () {
 	return 0
 }
 
-[[ ${DBG} == 'true' ]] && echo "ARGS:${#} ${@}" > debug.log
-[[ ${DBG} == 'true' ]] && echo "APP_DEPS:${APP_DEPS}" >> debug.log
-
-# Pre-scan - each module will add it's dependencies 
-# to _DEPS (defined in LIB_INIT) to be sourced by the post scan
-# TODO: This section is likely unecessary. The post-scan section
-# TODO: modification to perform multiple passes should suffice.
-for A in ${=APP_DEPS};do # Pre scan
-	FN=${A:t}
-	if is_valid ${FN};then
-		[[ ${DBG} == 'true' ]] && echo "Pre-scan Sourcing ${FN}" >> debug.log
-		source ${_LIB_DIR}/${FN}
-		[[ ${DBG} == 'true' ]] && echo "${FN}: _DEPS:${_DEPS}" >> debug.log
-		SEEN[${FN}]=1
-	fi
-done
-
-
-# Post-scan - source any modules in _DEPS
-# Multiple passes may be needed as previously
-# unseen dependencies are added to _DEPS
+# LIB_INIT defines the _DEPS array and pre-populates
+# it with default modules. Sourced modules then
+# append _DEPS with their dependencies.  Multiple 
+# passes resolve any additions to the _DEPS array if 
+# modified when modules are sourced
 MAX_DEPS=${#_DEPS}
 PASS=1
 while true;do
 	for D in ${_DEPS};do # Post scan
 		FN=${D:t}
 		if is_valid ${FN};then
-			[[ ${DBG} == 'true' ]] && echo "Post-scan pass ${PASS}: Sourcing ${FN}" >> debug.log
+			[[ ${_DBG} == 'true' ]] && echo "Dependency scan pass ${PASS}: Sourcing ${FN}" >> debug.log
 			source ${_LIB_DIR}/${FN}
 			SEEN[${FN}]=1
 		fi
 	done
-	[[ ${#_DEPS} -gt ${MAX_DEPS} ]] && MAX_DEPS=${#_DEPS} || break
+	[[ ${#_DEPS} -gt ${MAX_DEPS} ]] && MAX_DEPS=${#_DEPS} || break # _DEPS is stable - break
 	((PASS++))
 done
